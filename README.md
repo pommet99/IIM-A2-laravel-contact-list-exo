99aad9aa57aa4b6a0a95a7b5829b3240e5db17ad
Alexandre <alexandrepayen@yahoo.fr>
2020-02-07 12:59:12 +0100

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+#Installation : 
+
+* Dans un terminal à la racine du projet
+```
+composer install
+npm run watch
+
+//Dans un autre terminal
+php artisan serve
+
+```
+
+* Copier .env.example dans un fichier .env et remplir les informations de DB
+```
+php artisan migrate
+php artisan db:seed
+```
+
+
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!-- TODO Etendre le layout "app" -->
+
+<!-- TODO Dans la séction "content", ajouter un container, et dans ce container faire une liste des users et de leur nombre de contacts-->
+
+<div class="container">
+    <p>L'utilisateur "NOM" à "N" contact</p>
+    <!-- OU -->
+    <p>Il n'y a pas encore d'utilisateur</p>
+</div>
